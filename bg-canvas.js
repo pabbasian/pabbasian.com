7b80b74e7cc5f259e9157d6398e8162f84107ea1
Payman <pabbasian@gmail.com>
2017-01-06 18:38:14 +0100

    commit message

---
+++ bg-canvas.js
@@ -0,0 +1,57 @@
+function Bit(size, color) {
+    this.size = size;
+    if (size < 20) {
+        this.speed = 1.2;
+    } else if (size < 30) {
+        this.speed = .8;
+    } else {
+        this.speed = 0.5;
+    }
+    this.color = color;
+    this.xpos = Math.random() * screen.availWidth - this.size;
+    this.ypos = 100 + 500 * (canvas.width - this.xpos) / canvas.width + 100 * Math.sin(this.offset + this.xpos / 500);
+    this.offset = new Number(100 * Math.random());
+
+    this.tick = function() {
+        if (this.xpos > maxX) {
+            this.xpos = -this.size;
+            this.offset = new Number(100 * Math.random());
+        } else {
+            this.xpos += this.speed * Math.PI;
+        }
+        this.ypos = 100 + 500 * (canvas.width - this.xpos) / canvas.width + 100 * Math.sin(this.offset + this.xpos / 500) * Math.PI;
+    }
+
+    this.draw = function() {
+        context.fillStyle = this.color;
+        context.fillRect(this.xpos, this.ypos, this.size, this.size);
+        context.arc(this.xpos, this.ypos, this.size,0, this.size*Math.PI);
+
+    }
+} // end of bit object
+
+function reDraw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    for (var bit in bits) {
+        bits[bit].draw();
+        bits[bit].tick();
+    }
+}
+var colours = [
+        '#ff6978', '#b1ede8', '#ffcdb2 ', '#ffcdb2'
+    ],
+    maxX = screen.availWidth ;
+var canvas = document.getElementById('bg-canvas');
+canvas.width = screen.availWidth;
+canvas.height = screen.availHeight;
+var context = canvas.getContext('2d');
+context.globalAlpha = 0.5;
+
+var bits = new Array(10);
+var bit;
+for (var i = 0; i < 10; ++i) {
+    bit = new Bit(10 + 400 * Math.random(), colours[Math.floor(4 * Math.random())]);
+    bits[i] = bit;
+}
+
+setInterval(reDraw, 50);
